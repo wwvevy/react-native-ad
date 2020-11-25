81e2c1a0511d0e333a93669621676d77aa86c2c6
wuwei <592527542@qq.com>
2020-11-25 21:02:32 +0800

    feature: 开屏广告监听修改

--- src/Splash.ts
+++ src/Splash.ts
@@ -1,15 +1,11 @@
 import React from "react";
-import {
-	NativeModules,
-	NativeEventEmitter,
-	requireNativeComponent,
-} from "react-native";
+import { NativeModules, NativeEventEmitter } from "react-native";
 
 
 interface EVENT_TYPE {
 	onAdError: string; // 广告加载失败监听
 	onAdClick: string; // 广告被点击监听
-	onAdClose: string; // 广告关闭
+	onAdClose: string; // 广告时间结束关闭
 	onAdSkip: string; // 用户点击跳过广告监听
 	onAdShow: string; // 开屏广告开始展示
 }
@@ -28,8 +24,6 @@ export default ({ appid, codeid }) => {
 				listenerCache[type].remove();
 			}
 			return listenerCache[type] = eventEmitter.addListener("SplashAd-" + type, (event: any) => {
-				console.log('SplashAd event type ', type);
-				console.log('SplashAd event ', event);
 				callback(event);
 			});
 		}
